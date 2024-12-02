a1c9427ea1218402290e54bead5cf2feb3663344
TheresaDF <s194329@student.dtu.dk>
2024-12-02 13:18:49 +0100

    activate environment

--- batch_scripts/batchjob.sh
+++ batch_scripts/batchjob.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #  assert correct run dir
-run_dir="NEURONSPIKEEYE"
+run_dir="NeuronSpikeEye"
 if ! [ "$(basename $PWD)" = $run_dir ];
 then
     echo -e "\033[0;31mScript must be submitted from the directory: $run_dir\033[0m"
@@ -21,7 +21,7 @@ mkdir -p "logs/"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now 
 #BSUB -W 7:00
 ### -- request 5GB of system-memory --
-#BSUB -R "rusage[mem=8GB]"
+#BSUB -R "rusage[mem=5GB]"
 ### -- set the email address --
 ##BSUB -u s194329@student.dtu.dk
 ### -- send notification at start --
@@ -35,9 +35,7 @@ mkdir -p "logs/"
 ### -- end of LSF options --
 
 # activate env
-
-# load additional modules
-# module load cuda/11.4
+source neuron_spike/bin/activate 
 
 # run scripts
 python src/data/preprocess_data.py
