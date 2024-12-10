e36a76fc342a9ef94b1920cdc984475057889dd4
TheresaDF <s194329@student.dtu.dk>
2024-12-10 08:39:03 +0100

    replace entropy with acf

--- src/data/precprocess_utils.py
+++ src/data/precprocess_utils.py
@@ -2,7 +2,9 @@ from matplotlib import pyplot as plt
 import numpy as np
 from scipy.fft import fft, fftfreq, ifft 
 from sklearn.decomposition import FastICA  
-from sklearn.cluster import KMeans
+from scipy.stats import entropy
+from scipy.signal import butter, filtfilt
+from statsmodels.tsa.stattools import acf
 
 def time_to_freq(data, sample_rate = 3*1e4):
     """
@@ -39,16 +41,16 @@ def remove_ica_components(ica, data, idx):
     modified_data[:, idx] = 0 
     return ica.inverse_transform(modified_data)
 
-def remove_drift(bins_, freq = 1):
+def remove_drift(data, freq = 1):
     """
     Function to remove drift from the data
     """
-    bins = np.copy(bins_)
-    for i in range(100):
-        xf, yf = time_to_freq(bins[i])
+    bins = np.copy(data)
+    for i in range(bins.shape[1]):
+        xf, yf = time_to_freq(bins[:, i])
         yf[np.abs(xf) < freq] = 0 
         bin_ = np.real(freq_to_time(yf))
-        bins[i] = bin_
+        bins[:, i] = bin_
 
     return bins 
 
@@ -84,35 +86,24 @@ def compute_num_components(data, threshold = 0.95, to_plot = False) -> np.ndarra
 
     return idx
 
+
 def find_bad_channels(data : np.ndarray, n_comp : int) -> np.ndarray:
     """ Function to find bad channels using ICA"""
-    val = []
+
+    acfs_all = np.zeros(n_comp)
     for i in range(n_comp):
-        IQR = np.percentile(data[:, i], 75) - np.percentile(data[:, i], 25)   
-        val.append(IQR)
-
-    # normalize and fit kmeans 
-    val = np.array(val)
-    val = val / np.max(val)
-    kmeans = KMeans(n_clusters = 2)
-    kmeans.fit(np.c_[val, np.zeros(len(val))])
-    labels = kmeans.labels_
-
-    # find the indices from the class that is smallest (it may change between runs)
-    idx = np.where(labels == 0)[0] if len(np.where(labels == 0)[0]) < len(np.where(labels == 1)[0]) else np.where(labels == 1)[0]
-
-    return idx
-
-def low_pass(data : np.ndarray, freq_threshold : int = 10000, sampling_rate : int = 30000) -> np.ndarray: 
-    """Function to apply high pass filter"""
-    data_ = np.copy(data)
-    for i in range(data.shape[1]):
-        xf, yf = time_to_freq(data[:, i], sample_rate=sampling_rate)
-        yf[np.abs(xf) > freq_threshold] = 0
-
-        data_[:, i] = freq_to_time(yf)
+        acfs_all[i] = acf(data[:, i], nlags=20*30)[-1]
     
-    return data_ 
+    bad_component = np.max(np.abs(acfs_all))
+    return bad_component 
+
+
+def butter_lowpass(data : np.ndarray, cutoff : int = 5000, fs : int = 30000, order=10):
+    nyquist = 0.5 * fs
+    normal_cutoff = cutoff / nyquist
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return filtfilt(b, a, data, axis=0)
+
 
 
 def filter(data : np.ndarray) -> np.ndarray:
@@ -120,7 +111,7 @@ def filter(data : np.ndarray) -> np.ndarray:
 
     # remove drift 
     data = remove_drift(data)
-    data = low_pass(data, freq_threshold = 5000)
+    data = butter_lowpass(data)
 
     # compute number of components to keep 
     n_comp = max(7, compute_num_components(data, threshold = 0.95))
