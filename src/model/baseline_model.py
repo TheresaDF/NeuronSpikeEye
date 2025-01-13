7664db66f560be550a3c5df6d8fbffad4c6e862f
TheresaDF <s194329@student.dtu.dk>
2025-01-13 10:55:06 +0100

    changed function to work on real data as well

--- src/model/baseline_model.py
+++ src/model/baseline_model.py
@@ -19,16 +19,16 @@ def bin_data(channel : np.ndarray, peaks : list) -> np.ndarray:
     return binned_data
 
 
-def count_caps_baseline(simulator : SimulateData, filtered_signal : np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+def count_caps_baseline(orig_signal : np.ndarray, filtered_signal : np.ndarray, num_channels : int = 32, duration : int = 10, stim_freq : int = 10) -> tuple[np.ndarray]:
     """ Function that estimates the number of CAPs in the signal """
 
     # allocate memory for the counts
-    all_est_counts = np.zeros((simulator.num_channels, int(simulator.duration * simulator.stim_freq)))
+    all_est_counts = np.zeros((num_channels, int(duration * stim_freq)))
 
     # loop over all channels
-    for channel in tqdm(range(simulator.num_channels)):
+    for channel in tqdm(range(num_channels)):
         # find the SA and bin accordingly
-        peaks, _ = find_peaks(simulator.signal[:, channel], height = 30, distance = 300000 / (simulator.stim_freq * simulator.duration))
+        peaks, _ = find_peaks(orig_signal[:, channel], height = 30, distance = 300000 / (stim_freq * duration))
         bins = bin_data(filtered_signal[:, channel], peaks).T 
 
         # loop over all bins
