421a7e773c09578328fd61a5441826767c30df17
TheresaDF <s194329@student.dtu.dk>
2025-03-03 10:59:22 +0100

    change construction of save name

--- src/model/counter_real.py
+++ src/model/counter_real.py
@@ -19,15 +19,17 @@ def counter(args : tuple[str, str]) -> None:
 
     # unpack arguments 
     path, data_type = args
-    print(path, data_type)
     
     # construct save name 
     if data_type == "stim": 
-        save_name = f"../../../../../../../../work3/s194329/results_real_{data_type}/ramp{path.split("/")[-1].split("e")[-1].split(".")[0]}.pkl"
+        # save_name = f"../../../../../../../../work3/s194329/results_real_{data_type}/ramp{path.split("/")[-1].split("e")[-1].split(".")[0]}.pkl"
+        save_name = "../../../../../../../../work3/s194329/results_real_" + data_type + "/ramp" + path.split("/")[-1].split("e")[-1].split(".")[0] + ".pkl"
     elif data_type == "ttx": 
-        save_name = f"../../../../../../../../work3/s194329/results_real_{data_type}/eye{path.split(" ")[1][0]}.pkl"
+        # save_name = f"../../../../../../../../work3/s194329/results_real_{data_type}/eye{path.split(" ")[1][0]}.pkl"
+        save_name = "../../../../../../../../work3/s194329/results_real_" + data_type + "/eye" + path.split(" ")[1][0] + ".pkl"
     elif data_type == "spon": 
-        save_name = f"../../../../../../../../work3/s194329/results_real_{data_type}/eye{path.split("/")[5][-1]}_ramp{path.split("/")[-1].split(".")[0].split("e")[1]}.pkl"
+        # save_name = f"../../../../../../../../work3/s194329/results_real_{data_type}/eye{path.split("/")[5][-1]}_ramp{path.split("/")[-1].split(".")[0].split("e")[1]}.pkl"
+        save_name = "../../../../../../../../work3/s194329/results_real_" + data_type + "/eye" + path.split("/")[5][-1] + "_ramp" + path.split("/")[-1].split(".")[0].split("e")[1] + ".pkl"
 
     if os.path.exists(save_name): 
         print(f"Skipping save_name : {save_name}")
